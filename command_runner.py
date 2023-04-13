cffcc512190d8298b472ba816834d74cc03fb944
Hanief Nashrullah <68178470+hannoobz@users.noreply.github.com>
2023-04-13 19:56:19 +0700

    Update command_runner.py

--- command_runner.py
+++ command_runner.py
@@ -1,5 +1,14 @@
 from list_of_commands import *
+users = loader("user.csv") # Matriks data user
+candi = loader("candi.csv") # Matriks data candi
+bahan_bangunan = loader("bahan_bangunan.csv") # Data bahan bangunan
+role = ""
+username = ""
 
 def runner(string_command):
+    global role
+    global username
     if string_command == "login":
-        login()
+        role,username = login(users,role,username)
+    if string_command =="logout":
+        role = logout(role)
