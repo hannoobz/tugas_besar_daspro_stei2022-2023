693113abd45d2c126782c949e3c20ad880979716
Hanief Nashrullah <68178470+hannoobz@users.noreply.github.com>
2023-04-16 21:15:43 +0700

    ditambahkan keterangan cara pakai custom module

--- custom_module.py
+++ custom_module.py
@@ -1,5 +1,10 @@
 # Fungsi manual_split 
 # untuk splitting string
+# Cara pakai :
+# array = manual_split(array,"karakter split")
+# tidak bisa langsung :
+# manual_split(array,"karakter")
+# harus di assign ke variabel array terlebih dahulu
 def manual_split(csv_str,char):
     elmt = 0
     for i in range(len(csv_str)):
@@ -24,6 +29,10 @@ def manual_len(arr):
 
 # Fungsi loader
 # memuat file menjadi dalam bentuk array
+# Contoh format hasil return
+# [['username', 'password', 'role'], ['Bondowoso', 'cintaroro', 'bandung_bondowoso'], ['Roro', 'gasukabondo', 'roro_jonggrang'], ';EOP']
+# [['id', 'pembuat', 'pasir', 'batu', 'air'], ';EOP']
+# [['nama', 'deskripsi', 'jumlah'], ';EOP']
 def loader(directory):
     file = open(directory,"r")
     array = file.read()+";EOP"
@@ -35,6 +44,8 @@ def loader(directory):
 
 # Fungsi manual_append
 # pengganti append
+# Cara pakai :
+# array = manual_append(array,elemenbaru)
 def manual_append(array,element):
     length = manual_len(array)
     new_array = ["" for i in range(length+2)]
