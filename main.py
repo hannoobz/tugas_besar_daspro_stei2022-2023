0a81583218a2e1434f2b748b6b9a22451f631695
Hanief Nashrullah <68178470+hannoobz@users.noreply.github.com>
2023-05-01 23:34:11 +0700

    Added comment

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 # File: main.py
+
+# Import modul command_runner
 from command_runner import *
+
+# Import modul argparse 
 import argparse
 
+# Menerima argument dari terminal
 Load = argparse.ArgumentParser()
 Load.add_argument("folder_dir",nargs='?',type=str)
 LoadFileArgs = Load.parse_args()
 folder_dir = LoadFileArgs.folder_dir 
 
+# Menjalankan prosedur load 
+# dengan argument yang diterima
 loadgame(folder_dir)
 
+# Loop CLI
 while True:
     masukan = input(">>> ")
     runner(masukan)
