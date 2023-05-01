b8a04cc42bfc4b9298c42ab81ec6681eff4a735f
Filbert Fuvian <103139633+filbertfuvian@users.noreply.github.com>
2023-05-01 23:47:17 +0700

    Removed manual_pop and added comment

--- custom_module.py
+++ custom_module.py
@@ -55,21 +55,11 @@ def manual_append(array,element):
     new_array[length+1] = ";EOP"
     return new_array
 
-def manual_pop(array,element):
-    length = manual_len(array)
-    new_array = ["" for i in range(length)]
-    elmt_found = False
-    for i in range(length):
-        if array[i]==element:
-            elmt_found = True
-        if elmt_found==False:
-            new_array[i]=array[i]
-        else:
-            new_array[i]=array[i+1]
-    return new_array
-
-# Mengambil sebuah matriks dan hanya mengeluarkan row yang berisi data
-# (Menghapus row judul)
+# Fungsi removeFirstRow
+# untuk menghapus first elemen dari array
+# Cara pakai :
+# array = removeFirstRow(arr)
+# harus di assign ke variabel array terlebih dahulu
 def removeFirstRow(arr):
     length = manual_len(arr)
     arr_data = [0 for i in range(manual_len(arr))]
@@ -77,8 +67,13 @@ def removeFirstRow(arr):
         arr_data[i] = arr[i+1]
     return arr_data
     
-# Mengambil sebuah matriks dan integer kolom dan membuat array berisi
-# data pada kolom tersebut 
+# Fungsi getArrayCol
+# untuk membuat array 1 kolom dari matriks
+# Cara pakai :
+# array = getArrayCol(m,col)
+# m = matriks
+# col = kolom yang ingin dipilih
+# harus di assign ke variabel array terlebih dahulu
 def getArrayCol(m, col):
     arrCol = [0 for i in range(manual_len(m)+1)]
     for i in range(manual_len(m)):
@@ -86,9 +81,13 @@ def getArrayCol(m, col):
     arrCol[manual_len(m)] = ';EOP'
     return(arrCol)
 
-# Mengambil sebuah matriks, integer baris, dan range kolom, lalu 
-# membuat array berisi data pada baris tersebut dengan ketentuan pada
-# range kolom yang dimasukan
+# Fungsi getArrayRow
+# untuk membuat array row dengan batas kolom
+# Cara pakai :
+# array = getArrayRow(arr,row,col1,col2)
+# row = row yang dipilih
+# col1,col2 = batasan
+# harus di assign ke variabel array terlebih dahulu
 def getArrayRow(arr, row, col1, col2):
     arrRow = [0 for i in range(col2-col1+1)]
     for i in range(col1,col2):
@@ -96,14 +95,24 @@ def getArrayRow(arr, row, col1, col2):
     arrRow[col2-col1] = ";EOP"
     return(arrRow)
 
-# Mengeluarkan jumlah dari semua elemen dalam array integer (RECURSIVE)
+# Fungsi sumArray
+# untuk menghitung jumlah integer dalam array integer
+# Cara pakai :
+# sum = sumArray(arr)
+# Fungsi ini bersifat rekursif
 def sumArray(arr):
     if manual_len(arr) == 0:
         return 0
     else:
         return int(arr[0]) + sumArray(removeFirstRow(arr))
 
-# Mengeluarkan ID integer maksimum dalam sebuah array integer (RECURSIVE)
+# Fungsi find_max_id
+# untuk mencari id tertinggi dari array of integer
+# Cara pakai :
+# maxid = find_max_id(arr,0,1)
+# max_id = 0 karena id yang pertama di tes adalah id 0
+# idx = 1 karena id yg ingin dibandingkan pertama adalah id 1
+# Fungsi ini rekursif
 def find_max_id(arr, max_id, idx):
     if idx == manual_len(arr):
         return max_id
@@ -112,7 +121,13 @@ def find_max_id(arr, max_id, idx):
             max_id = idx
         return find_max_id(arr, max_id, idx + 1)
 
-# Mengeluarkan ID integer minimum dalam sebuah array integer (RECURSIVE)
+# Fungsi find_max_id
+# untuk mencari id terendah dari array of integer
+# Cara pakai :
+# minid = find_min_id(arr,0,1)
+# min_id = 0 karena id yang pertama di tes adalah id 0
+# idx = 1 karena id yg ingin dibandingkan pertama adalah id 1
+# Fungsi ini rekursif
 def find_min_id(arr, min_id, idx):
     if idx == manual_len(arr):
         return min_id
@@ -121,11 +136,19 @@ def find_min_id(arr, min_id, idx):
             min_id = idx
         return find_min_id(arr, min_id, idx + 1)
 
-# Mengeluarkan huruf pertama dari sebuah character
+# Fungsi firstLetter
+# untuk mengeluarkan huruf pertama
+# Cara pakai :
+# firstletter = firstletter(char)
 def firstLetter(char):
     return char[0]
 
-# Menguluarkan banyaknya x muncul dalam array (RECURSIVE)
+# Fungsi howManyX
+# untuk menghitung jumlah X muncul dalam array
+# Cara pakai :
+# count = howManyX(arr,x,0)
+# parameter count = 0 karena awalnya dihitung dimulai dari 0
+# Fungsi ini bersifat rekursif
 def howManyX(arr, x, count):
     if manual_len(arr) == 0:
         return count
@@ -133,12 +156,22 @@ def howManyX(arr, x, count):
         count += 1
     return howManyX(removeFirstRow(arr), x, count)
 
+# Fungsi findIdx
+# untuk mencari index dimana x ditemukan pada array
+# Cara pakai :
+# idx = findIdx(arr,x)
+# Akan mereturn boolean False jika x tidak ditemukan
 def findIdx(arr,x):
     for i in range(manual_len(arr)):
         if arr[i] == x:
             return i
     return False
 
+# Fungsi removeElmt
+# untuk menghilangkan Elmt pada id tertentu
+# Cara pakai :
+# arr = removeElmt(arr,id)
+# harus di assign ke variabel
 def removeElmt(arr,id):
     new_arr = [0 for i in range(manual_len(arr))]
     c = 0
@@ -149,7 +182,11 @@ def removeElmt(arr,id):
     new_arr[c] = ";EOP"
     return new_arr
 
-# arrtocsv, memasukkan array ke file csv
+# Fungsi arrtocsv
+# memasukkan array ke file csv
+# Cara pakai :
+# arrtocsv(arr,filename,col)
+# Tidak perlu di assign ke variabel
 def arrtocsv(arr,filename,col):
     csv_file = open(filename,"w")
     for i in range(manual_len(arr)):
